29a36fd49bd2abeda08d08b269aa0b2557523502
Chad Fawcett <me@chadf.ca>
2017-10-27 16:50:55 -0700

    Allow user to specify docker repo

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const packageVersion = require('./package.json').version
 const exec = util.promisify(require('child_process').exec)
 
 ;(async () => {
-  const { stdout } = await exec("curl 'https://registry.hub.docker.com/v2/repositories/library/rethinkdb/tags/'")
+  const repo = process.argv[2]
+  const { stdout } = await exec(`curl 'https://registry.hub.docker.com/v2/repositories/${repo}/tags/'`)
 
   const tags = JSON.parse(stdout).results.map(i => i.name)
 
-  if (tags.includes(packageVersion)) {
+  if (tags.includes(packageVersion) || tags.includes('v' + packageVersion)) {
     console.error('Please update the package version before publishing.')
     process.exit(1)
   }
