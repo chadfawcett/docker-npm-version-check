bc844b75c8d5d01387f093a76f70a8bc1b092d7e
Chad Fawcett <me@chadf.ca>
2017-10-27 16:41:11 -0700

    Check if package version is contained in last 10 versions

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('Hello World!')
+const util = require('util');
+const packageVersion = require('./package.json').version
+const exec = util.promisify(require('child_process').exec)
+
+;(async () => {
+  const { stdout } = await exec("curl 'https://registry.hub.docker.com/v2/repositories/library/rethinkdb/tags/'")
+
+  const tags = JSON.parse(stdout).results.map(i => i.name)
+
+  if (tags.includes(packageVersion)) {
+    console.error('Please update the package version before publishing.')
+    process.exit(1)
+  }
+})()
